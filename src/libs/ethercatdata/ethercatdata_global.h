bf9d680da3bc0891a6e5c04fea464556c39d1c61
kvell <kong_w@foxmail.com>
2026-08-14 12:37:37 +0800

    EtherCATData: avoid importing MSVC value members
    Do not mark inline special members of EtherCAT value aggregates as
    `dllimport`. MSVC otherwise expects symbols that the data DLL never
    emits when those implicit members are unused inside the library.

--- src/libs/ethercatdata/ethercatdata_global.h
+++ src/libs/ethercatdata/ethercatdata_global.h
@@ -8,6 +8,11 @@
 #define ETHERCATDATA_EXPORT Q_DECL_EXPORT
 #elif defined(ETHERCATDATA_STATIC_LIBRARY)
 #define ETHERCATDATA_EXPORT
+#elif defined(Q_CC_MSVC)
+// Keep inline special members of value types local to MSVC consumers. Importing the entire
+// aggregate makes MSVC expect DLL symbols that are never emitted when the library does not use
+// those implicitly generated members itself.
+#define ETHERCATDATA_EXPORT
 #else
 #define ETHERCATDATA_EXPORT Q_DECL_IMPORT
 #endif
